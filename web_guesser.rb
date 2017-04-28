363d546b88850fc8642ed21d42d7ca2adc344a3a
MasonHolland <masontholland@gmail.com>
2017-04-27 18:28:01 -0600

    meets requirements and 1 extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,22 +3,36 @@ require 'sinatra/reloader'
 
 
 def check_guess(guess, number)
-    if guess.to_i > number
+    if guess > number
       message = "Too high!"
-        message = "Way too high!" if guess.to_i > (number + 5)
-    elsif guess.to_i < number
+        message = "Way too high!" if guess > (number + 5)
+    elsif guess < number
       message = "Too low!"
-        message = "Way too low!" if guess.to_i < (number - 5)
+        message = "Way too low!" if guess < (number - 5)
     else
       message = "You got it right! \n The SECRET number is #{number}"
     end
   message
 end
 
+def change_colour(guess, number)
+  if guess < number
+    colour = "crimson"
+      colour = "darkred" if guess < (number - 5)
+  elsif guess > number
+    colour = "crimson"
+        colour = "darkred" if guess > (number + 5)
+  else
+    colour = "green"
+  end
+  colour
+end
+
 number = rand(100)
 
 get '/' do
-  guess = params["guess"]
+  guess   = params["guess"].to_i
   message = check_guess(guess, number)
-  erb :index, :locals => {:message => message}
+  colour  = change_colour(guess, number)
+  erb :index, :locals => {:message => message, :colour => colour}
 end
